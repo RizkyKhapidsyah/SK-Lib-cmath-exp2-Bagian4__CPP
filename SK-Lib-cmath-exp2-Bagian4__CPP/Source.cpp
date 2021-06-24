b831f0643244060b9a04136ecdc1bc88ee3cc425
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-24 16:19:24 +0700

    Add project files.

---
+++ SK-Lib-cmath-exp2-Bagian4__CPP/Source.cpp
@@ -0,0 +1,21 @@
+// C++ program to illustrate the
+// exp2() function for 0
+#include <cmath>
+#include <iostream>
+#include <conio.h>
+
+/*	Source by GeeksForGeeks
+	Modified For Learn by Rizky Khapdisyah
+	I.D.E : VS2019 */
+
+using namespace std;
+
+int main() {
+	int n = 0;
+
+	int ans = exp2(n);
+	cout << "exp2(0) = " << ans << endl;
+
+	_getch();
+	return 0;
+}
